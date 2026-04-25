ea1087ecdaaf4e3649c2b91496b78a88954af61c
CharlyCharly00 <ian.ortega@walmart.com>
2026-04-24 18:36:59 -0600

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -19,10 +19,11 @@ st.title("Mapa logístico con filtros")
 @st.cache_data
 def load_data():
     sharepoint_url = (
-        "https://teams.wal-mart.com/:x:/r/sites/ICO0008UpdateBI/Documentos%20compartidos/DashboardMOM/Points_map.csv?d=we32b0f27a3e242fba186b6537d6e1177&csf=1&download=1"
+        "https://teams.wal-mart.com/:x:/r/sites/ICO0008UpdateBI/Documentos%20compartidos/DashboardMOM/Points_map.csv?d=we32b0f27a3e242fba186b6537d6e1177&download=1"
     )
 
-    df = pd.read_csv(sharepoint_url, low_memory=False)
+    
+df = pd.read_csv(sharepoint_url, low_memory=False)
 
     df.columns = df.columns.str.strip()
 
@@ -34,6 +35,7 @@ def load_data():
     df = df.dropna(subset=["LATITUDE_DRIVER", "LONGITUDE_DRIVER"])
 
     return df
+
 
 df = load_data()
 
